64b69ba1a42a5e117864dcf2578411b341b6b6d7
Devansh Sharma <109816489+Devansh222@users.noreply.github.com>
2024-06-21 20:18:50 +0530

    updated files

--- chaiheadq/tweet/urls.py
+++ chaiheadq/tweet/urls.py
@@ -1,11 +1,14 @@
 
 from django.urls import path
+from django.contrib import admin
 
 from .import views
 
 urlpatterns = [
     # path('', views.index,name='index'),
+    
     path('', views.tweet_list,name='tweet_list'),
+    path('admin/', admin.site.urls),
     path('create/', views.tweet_create,name='tweet_create'),
     path('<int:tweet_id>/edit/', views.tweet_edit,name='tweet_edit'),
     path('<int:tweet_id>/delete/', views.tweet_delete,name='tweet_delete'),
